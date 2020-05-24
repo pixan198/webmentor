1433e8143de42ef675c1ce2d88adc256ddfef777
Surbhi Jain <44497036+Surbhijn123@users.noreply.github.com>
2020-05-24 16:49:47 +0530

    Update Js file

--- Surbhijn123/task4/js/script.js
+++ Surbhijn123/task4/js/script.js
@@ -1,61 +1,65 @@
+"use strict";
+
 //Global Variables
-var calBtn = document.getElementsByClassName('btn');
-var result = document.getElementById('result');
+const calBtn = document.getElementsByClassName('btn');
+let result = document.getElementById('result');
 
 //resetNo variable will be use after calculating the arithmetic expression we will reset the number so, number will not concate to the result.
-var resetNo = false;
+let resetNo = false;
 
 //decimalCount variable will help to prevent the occurrence  of multiple decimal point after digit.  
-var decimalCount = 0;
+let decimalCount = 0;
 
 //Add Keypress event for user (If user input from keyboard)
 document.addEventListener('keypress', perform);
 
 //Add click event to all calculator button
-for(i=0; i< calBtn.length;i++) {
+for(let i = 0; i < calBtn.length; i++) {
    calBtn[i].addEventListener('click', perform);
 }
 
 function perform() {
 
-	//The value enter by user will store in "userValues"
-	if(event.type == 'keypress') {
+	//The value enter by user will store in "userValue"
+	let userValue;
+	if(event.type === 'keypress') {
 		//To prevent browser default behavior because "Enter and space" key fire both the events 
 		event.preventDefault();
 		
 		//If space key is eneterd
-		if(event.keyCode == 32)
-		var userValue = '';
+		if(event.keyCode === 32)
+		userValue = '';
 		else
-		var userValue = event.key;
+		userValue = event.key;
 	} else
-	var userValue = this.value;
+		userValue = this.value;
 
 	//If value is numeric
 	if(!isNaN(userValue)) {
-		if(resetNo == true) {
+		if(resetNo === true) {
 			result.value = userValue;
 			resetNo = false;
 		}
 		else
 			result.value = result.value + userValue;
 	
-	} else if(userValue == '/' || userValue == '*' || userValue == '+' || userValue == '-' || userValue == '%') {
+	} else if(userValue === '/' || userValue === '*' || userValue === '+' || userValue === '-' || userValue === '%') {
 		
 		resetNo = false;
 		decimalCount = 0;
-		var lastChar = fetchLastCharacter(result.value);
+		const lastChar = fetchLastCharacter(result.value);
 
 		if(['/','*','+','-','%','.'].includes(lastChar))
 			result.value = exceptLastChar(result.value) + userValue;//replace the last arthimetic operator
 		else
 			result.value = result.value + userValue;
 	
-	} else if(userValue == '.') {
+	} else if(userValue === '.') {
 
-		if(decimalCount == 0) {
-			var lastChar = fetchLastCharacter(result.value);
-			if(resetNo == true) {
+		if(decimalCount === 0) {
+
+			const lastChar = fetchLastCharacter(result.value);
+			if(resetNo === true) {
 
 				result.value = userValue;
 				resetNo = false;
@@ -68,39 +72,38 @@ function perform() {
 
 		decimalCount++;
 
-	} else if(userValue == 'power') {
+	} else if(userValue === 'power') {
 		try {
-				var expRes = math.evaluate(result.value);
+				const expRes   = math.evaluate(result.value);
 				result.value = math.pow(expRes,2);
-				resetNo = true;
-
+				resetNo      = true;
 		} catch (err) {
 				result.value = err;
 		}
 		disableCalcBtn(result.value);	
 
-	} else if(userValue == '=' || userValue == 'Enter') {
+	} else if(userValue === '=' || userValue === 'Enter') {
 		try {
 				result.value = math.evaluate(result.value);
-				resetNo = true;
+				resetNo      = true;
 				decimalCount = 0;
-			} catch (err) {
+		} catch (err) {
 				result.value = err;
-			}
-			disableCalcBtn(result.value);
+		}
+		disableCalcBtn(result.value);
 
-	} else if(userValue == 'Backspace') {
+	} else if(userValue === 'Backspace') {
 
-		var lastChar = fetchLastCharacter(result.value);
-		if(lastChar == '.')
+		const lastChar = fetchLastCharacter(result.value);
+		if(lastChar === '.')
 			decimalCount = 0;
-		if(resetNo == false)
+		if(resetNo === false)
 		result.value = exceptLastChar(result.value);
 
-	} else if(userValue == 'clear') {
+	} else if(userValue === 'clear') {
 
-		if(result.value == 'Overflow' || result.value == 'Infinity' || isNaN(result.value)) {
-			for(i=0; i< calBtn.length;i++) {
+		if(result.value === 'Overflow' || result.value === 'Infinity' || isNaN(result.value)) {
+			for(let i = 0; i < calBtn.length; i++) {
 				   calBtn[i].disabled = false;
 			}
 		}
@@ -115,15 +118,15 @@ function fetchLastCharacter(str) {
 }
 
 function exceptLastChar(str) {
-	if(str.length-1 == 0)
+	if(str.length-1 === 0)
 		return 0;
 	else
     return str.substring(0,str.length - 1);
 }
 
 function disableCalcBtn(str) {
-	if(str == 'Overflow' || str == 'Infinity' || isNaN(str)) {
-		for(i=0; i< calBtn.length;i++) {
+	if(str === 'Overflow' || str === 'Infinity' || isNaN(str)) {
+		for(let i = 0; i < calBtn.length; i++) {
 			   calBtn[i].disabled = true;
 		}
 		document.getElementById('clear').disabled = false;
