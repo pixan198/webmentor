604d22b0b7fe88fba19eaa13462523d82d4975cb
Ashish Chawda <ashish11chawda@gmail.com>
2020-05-29 22:12:21 +0530

    Add user input in task2

--- pixan198/task4/script.js
+++ pixan198/task4/script.js
@@ -1,15 +1,27 @@
-const second = 1000,
+
+
+
+function display(){
+	const second = 1000,
 	minute = second * 60,
 	hour = minute * 60,
 	day = hour * 24;
+let inpdate= new Date(document.getElementById("inpdate").value);
 
-let countDown = new Date('May 15, 2021 00:00:00').getTime();
-let x = setInterval(function() {    
-	let now = new Date().getTime(),
+let countDown = new Date(inpdate).getTime();
+let now = new Date().getTime(),
 	distance = countDown - now;
-
+	if(distance <= 0){
+		document.getElementById("lis").style.display="none";
+		document.getElementById("err").style.display="block";
+	}
+	else{
+		document.getElementById("lis").style.display="block";
+		document.getElementById("err").style.display="none";
 	document.getElementById('days').innerText = Math.floor(distance / (day)),
 	document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
 	document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
 	document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-}, second)
+	}
+let x = setInterval(display, 1000);
+}
